0cf2488773551dba61c1a9129fcd75f535c4b64c
Re-Angelo Jarvis <hesusdios@gmail.com>
2020-05-07 00:04:11 +0200

    first-commit

---
+++ src/pages/_app.js
@@ -0,0 +1,1 @@
+import React from 'react'import App from 'next/app'import '../styles/App/App.scss'class MyApp extends App {    render() {        const {Component, pageProps, router} = this.props;        return (                    <Component {...pageProps} key={router.route}/>        )    }}export default MyApp
