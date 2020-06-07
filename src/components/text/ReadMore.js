a3267ef8478b9139ec10735d4fda8204e11ba5c5
Re-Angelo Jarvis <hesusdios@gmail.com>
2020-06-07 17:16:30 +0200

    Disable mouse on link hover

--- src/components/text/ReadMore.js
+++ src/components/text/ReadMore.js
@@ -1,38 +1,57 @@
-import React from 'react';
-import Link from "next/link";
+import React, { useContext, useState } from 'react'
+import Link from 'next/link'
 import styled from '@emotion/styled'
+import { MouseContext } from '../../context/animations/MouseContext'
 
 const LinkText = styled('a')`
           color: ${props => props.inverted ? props.theme.fonts.white : props.theme.fonts.title} ;
         &:after{
           background-color: ${props => props.inverted ? props.theme.fonts.white : props.theme.fonts.title} ;
         }
-`;
+`
 
 const Text = styled('p')`
           color: ${props => props.inverted ? props.theme.fonts.white : props.theme.fonts.title} !important;
         &:after{
           background-color: ${props => props.inverted ? props.theme.fonts.white : props.theme.fonts.title} ;
         }
-`;
+`
 
+function ReadMore ({ to, inverted, text, action, margin, children, small, ...rest }) {
 
-function ReadMore({to, inverted, text, action, margin, children, small, ...rest}) {
-    return (
+	const mouse = useContext(MouseContext)
 
-        action ? (<Text inverted={inverted} {...rest}
-                        className={`read-more ${margin ? "margin" : "no-margin"} ${small && "small"}`}>
-                {children}
-            </Text>)
-            :
-            (
-                <Link href={to}>
-                    <LinkText inverted={inverted} className={`read-more ${margin && "margin"} ${small && "small"}`}>
-                        {children}
-                    </LinkText>
-                </Link>)
+	const hoverText = () => {
+		setTextBorder(true)
+		mouse.onHover('disabled')
+	}
 
-    );
+	const leaveText = () => {
+		setTextBorder(false)
+		mouse.onLeave()
+	}
+
+	const [textBorder, setTextBorder] = useState(false)
+
+	return (
+
+		action ? (
+				<Text inverted={inverted} {...rest} onMouseOver={() => {hoverText()}} onMouseLeave={() => {leaveText()}}
+					  className={`read-more ${margin ? 'margin' : 'no-margin'} ${small && 'small'} ${textBorder &&
+					  'border'}`}>
+					{children}
+				</Text>)
+			:
+			(
+				<Link href={to}>
+					<LinkText inverted={inverted}  onMouseOver={() => {hoverText()}} onMouseLeave={() => {leaveText()}}
+							  className={`read-more ${margin && 'margin'} ${small && 'small'} ${textBorder &&
+							  'border'}`}>
+						{children}
+					</LinkText>
+				</Link>)
+
+	)
 }
 
-export default ReadMore;
+export default ReadMore
