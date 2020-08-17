2946ff25bb0d4fbd4e29c6fa99f63db3f3fd316d
Amar97 <amarramdas97@gmail.com>
2020-08-17 17:19:21 +0200

    Finish why asrr in english

--- src/components/why-asrr/Why.js
+++ src/components/why-asrr/Why.js
@@ -9,27 +9,27 @@ import Email from "@/icons/Email";
 import Button from "@/Button";
 
 function Why() {
-    const [card, setCard] = useState(0);
-    const i18n = useI18n();
-    const values = ["Cheap", "Fast", "Agile"];
-    const darkmode = useTheme().dark;
+	const [card, setCard] = useState(0);
+	const i18n = useI18n();
+	const values = ["Cheap", "Fast", "Agile"];
+	const darkmode = useTheme().dark;
 
-    return (
-            <Section>
-                <Title title={'why.title.header'}/>
-                <div className="why-asrr">
-                    <div className="why-asrr-points why-asrr-center">
-                        {values.map((d, i) => <span onClick={() => setCard(i)} className={`${i === card && "selected-line"}`}><p className={`${!darkmode ? "animated-link-dark" : "animated-link-light"}`}>{d}</p></span>)}
-                    </div>
+	return (
+		<Section>
+			<Title title={'why.title.header'}/>
+			<div className="why-asrr">
+				<div className="why-asrr-points why-asrr-center">
+					{values.map((d, i) => <span onClick={() => setCard(i)}
+					                            className={`${i === card && "selected-line"}`}><p
+						className={`${!darkmode ? "animated-link-dark" : "animated-link-light"}`}>{i18n.t('home.why.' + i + '.title')}</p></span>)}
+				</div>
 
-                    <div className="why-asrr-text why-asrr-center">
-                        {card === 0 ? <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,</p> : null}
-                        {card === 1 ? <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. o ligula eget dolor. Aenean massa. Cum sociis natoque penatibus e Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,</p> : null}
-                        {card === 2 ? <p>Lorem ipsum dolor sit amet, consectetupenatibus et magnis dultricies nec, pellentesque eu,</p> : null}
-                    </div>
-                </div>
-            </Section>
-    )
+				<div className="why-asrr-text why-asrr-center">
+					<p>{i18n.t('home.why.' + card + '.text')}</p>
+				</div>
+			</div>
+		</Section>
+	)
 }
 
 export default Why
