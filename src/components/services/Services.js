b39669d04c56de6918193946495dd8490b22cd8d
Amar Ramdas <amarramdas@asrr.nl>
2020-11-01 17:24:29 +0100

    Create dynamic paragraphs in cards comp

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -6,19 +6,29 @@ import analytics from '#/services/data-analytics/data-analytics-square.jpg'
 import scale from '#/services/scaleable-software/scaleable-software-sqaure.jpg'
 import itaas from '#/services/itaas/itaas.jpg'
 import Fade from 'react-reveal/Fade';
+import useI18n from "../../hooks/use-i18n";
 
-const Card = ({image, text, title, to}) => {
+const Card = ({image, basePath, paragraphs}) => {
+    let title = `${basePath}.title`
 
+    const i18n = useI18n();
     return (
         <Fade bottom>
             <div className="service-card">
                 <div className="service-card-text">
                     <div className="service-card-text-wrapper">
 					<span>
-						<h2>Scaleable Software </h2>
-						<p>This is a text for you to see how far the button can
-						actually render if i try and some more random text to display on this page</p>
-                        {/*<Button to="/"/>*/}
+						{title && <h2>{i18n.t(title) !== undefined ? i18n.t(title) : title}</h2>}
+
+
+                        {[...Array(paragraphs)].map((x, i) => {
+
+                                let text = `${basePath}.paragraphs.${i}`;
+
+                                return <p>{i18n.t(text) !== undefined ? i18n.t(text) : text}</p>
+                            }
+                        )}
+
 					</span>
                     </div>
                 </div>
@@ -42,10 +52,10 @@ function Services() {
         <Section id="services">
             <Title basePath={'services.preview.header'}/>
             <div className="services">
-                <Card basePath={`services.bridge`} image={automation}/>
-                <Card basePath={`services.itaas`} image={itaas}/>
-                <Card basePath={`services.microservice`} image={scale}/>
-                <Card basePath={`services.insights`} image={analytics}/>
+                <Card basePath={`services.cards.bridge`} paragraphs={2} image={automation}/>
+                <Card basePath={`services.cards.itaas`} image={itaas}/>
+                <Card basePath={`services.cards.microservice`} image={scale}/>
+                <Card basePath={`services.cards.insights`} image={analytics}/>
             </div>
 
         </Section>
