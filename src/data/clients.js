efa4917098b7368dfc1f286483bfb71f0302e2c6
Amar Ramdas <amarramdas@asrr.nl>
2020-11-03 17:52:49 +0100

    Clean up case

--- src/data/clients.js
+++ src/data/clients.js
@@ -3,7 +3,6 @@ import NWO from '@/logos/NWO'
 import Hes from '@/logos/Hes'
 import React from 'react'
 
-//TODO: Fix issue with FORM logo
 export const clients = [
     {logo:<Form/>, alt:"form"},
     {logo:<NWO/>, alt:"nwo"},
