9e221adab4b0543e2fcf81ed325d622ab2927e4c
Amar Ramdas <amarramdas@asrr.nl>
2020-11-12 11:36:21 +0100

    Rename ittaas

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Section from '../layout/Section'
-import Title from '../titles/Title'
 import automation from '#/services/automation/automation-square.jpg'
 import analytics from '#/services/data-analytics/data-analytics-square.jpg'
 import scale from '#/services/scaleable-software/scaleable-software-sqaure.jpg'
-import itaas from '#/services/itaas/itaas.jpg'
 import Fade from 'react-reveal/Fade';
 import styled from "@emotion/styled";
 import Card from "@/services/Card";
 import PreviewServices from "@/services/Preview";
-import Application from "@/layout/Application";
 
 const Wrapper = styled(Section)`
         background: ${props => props.theme.layout};
@@ -25,19 +22,19 @@ function Services() {
 
                 <Fade delay={0} bottom>
                     <div className="service-card-wrapper card-margin-bottom">
-                        <Card route="/itaas" basePath={`services.cards.itaas`} paragraphs={1} image={scale}/>
+                        <Card route="/ittaas" basePath={`services.cards.itaas`} paragraphs={1} image={scale}/>
                     </div>
                 </Fade>
 
                 <Fade delay={200} bottom>
                     <div className="service-card-wrapper card-margin-bottom">
-                        <Card route="/" basePath={`services.cards.hardware`} paragraphs={2} image={automation}/>
+                        <Card route="/hardware" basePath={`services.cards.hardware`} paragraphs={2} image={automation}/>
                     </div>
                 </Fade>
 
                 <Fade delay={300} bottom>
                     <div className="service-card-wrapper card-margin-bottom">
-                        <Card route="/" basePath={`services.cards.bi`} paragraphs={1} image={analytics}/>
+                        <Card route="/bi" basePath={`services.cards.bi`} paragraphs={1} image={analytics}/>
                     </div>
                 </Fade>
 
--- src/data/services.js
+++ src/data/services.js
@@ -1,7 +1,7 @@
 export const services = [
     {
         type: "service",
-        name: "itaas",
+        name: "ittaas",
         readMore: true,
         landing: {
             backgroundImage: {
