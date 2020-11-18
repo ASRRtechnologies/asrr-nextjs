1c910f3bd1f50af08e0c6f82155ddabc68add9db
Amar Ramdas <amarramdas@asrr.nl>
2020-11-18 14:25:36 +0100

    Add images for bi hardware and update services

--- src/data/services.js
+++ src/data/services.js
@@ -45,5 +45,95 @@ export const services = [
             }
         ],
     },
+    {
+        type: "service",
+        name: "bi",
+        readMore: true,
+        landing: {
+            backgroundImage: {
+                extension: 'webp'
+            }
+        },
+        sections: [
+            {
+                title: true,
+                length: 1,
+                image: {
+                    extension: 'webp'
+                }
+                // iframe: {
+                //     code: "<iframe src=\"https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6719531516357091328\" height=\"692\" width=\"504\" frameborder=\"0\" allowfullscreen=\"\" title=\"Ingevoegde bijdrage\"></iframe>"
+                // }
+            },
+            {
+                type: "quote",
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                type: "bullets",
+                length: 5
+            }
+        ],
+    },
+    {
+        type: "service",
+        name: "hardware",
+        readMore: true,
+        landing: {
+            backgroundImage: {
+                extension: 'webp'
+            }
+        },
+        sections: [
+            {
+                title: true,
+                length: 1,
+                image: {
+                    extension: 'webp'
+                }
+                // iframe: {
+                //     code: "<iframe src=\"https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6719531516357091328\" height=\"692\" width=\"504\" frameborder=\"0\" allowfullscreen=\"\" title=\"Ingevoegde bijdrage\"></iframe>"
+                // }
+            },
+            {
+                type: "quote",
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                title: true,
+                length: 1,
+            },
+            {
+                type: "bullets",
+                length: 5
+            }
+        ],
+    },
 ];
 
