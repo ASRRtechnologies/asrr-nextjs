c537a6c24b5a5771a0421bc8d08f42a1c8b45561
ReangeloJ <hesusdios@gmail.com>
2020-11-19 00:46:34 +0100

    remove padding client section

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -4,7 +4,7 @@ import styled from '@emotion/styled'
 import Title from '@/titles/Title'
 import Section from '@/layout/Section'
 import Ticker from 'react-ticker'
-import Fade from "react-reveal";
+import Fade from "react-reveal/Fade";
 import uuid from 'react-uuid'
 
 const Wrapper = styled(Section)`
@@ -22,14 +22,14 @@ const Card = ({ children }) => {
 
 function Clients (props) {
 	return (
-		<Wrapper>
-			<Fade animation="fade-up" delay={200}>
+		<Wrapper className="clients-section">
+			<Fade bottom delay={200}>
 				<Title className="title-button" basePath={'clients.header'} compact noSection/>
-				<Wrapper className="clients">
+				<div className="clients">
 					<Ticker speed={2} mode="chain">
 						{({ index }) => (clients.map((d, i) => <Card key={uuid()} children={d.logo}/>))}
 					</Ticker>
-				</Wrapper>
+				</div>
 			</Fade>
 		</Wrapper>
 
