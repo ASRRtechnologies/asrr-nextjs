575f25c465dc7b90edfa84fcaf4c1bd93ac9d235
Amar Ramdas <amarramdas@asrr.nl>
2021-01-07 13:37:19 +0100

    Update CMS

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 const withImages = require('next-images');
 const withVideos = require('next-videos')
 
-module.exports = withImages(withVideos());
+module.exports = withImages(withVideos({
+    webpack: (cfg) => {
+        cfg.module.rules.push({
+            test: /\.md$/,
+            use: 'raw-loader',
+        })
+        return cfg;
+    }
 
-
+    // shallowRender: true,  <-- enable this in production mode wont call getInitialprops on every page render
+}))
 // module.exports = withImages({
 //     experimental: {
 //         redirects() {
