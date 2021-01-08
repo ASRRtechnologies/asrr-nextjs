24b898959f7a90f6f082953ac768ef976abda362
Re-Angelo Jarvis <hesusdios@gmail.com>
2021-01-08 03:06:04 +0100

    Add begin code to get single file

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,24 @@ import Clients from '../components/clients/Clients'
 import Contact from '../components/contact/Preview'
 import Stories from '@/stories/Stories'
 import Why from '@/why-asrr/Why'
-import Application from '../components/layout/Application'
 import PreviewBlog from '@/blog/Preview'
 import PreviewServices from "@/services/Preview";
 import PreviewPortfolio from "@/portfolio/Preview";
 import {useHeader} from "../context/navigation/HeaderContext";
 import TechStack from '@/techstack/TechStack'
+
+import fs from 'fs'
+import { join } from 'path'
+import matter from 'gray-matter'
+
+const homeDirectory = join(process.cwd(), 'public', 'content', 'pages', 'nl', 'home');
+export function getDocBySlug(slug) {
+    const realSlug = slug.replace(/\.md$/, '')
+    const fullPath = join(homeDirectory, `${realSlug}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const { data, content } = matter(fileContents)
+    return {data, content};
+ }
 
 function Index(props) {
     const header = useHeader();
@@ -31,6 +43,14 @@ function Index(props) {
         </>
 
     )
+}
+
+export async function getStaticProps () {
+    const data = getDocBySlug("homepage")
+
+    return {
+        props: { data },
+    }
 }
 
 export default Index
