e8a85f84d37d8ffb875e5ea9b9f85cccf7282ad1
Re-Angelo Jarvis <hesusdios@gmail.com>
2021-01-13 18:49:06 +0100

    Add privacy and algemene voorwaardne files, change card animation fade

--- src/components/contact/Map.js
+++ src/components/contact/Map.js
@@ -3,11 +3,7 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 import logo from 'public/assets/images/logo/asrr-logo-spacing-small.svg'
 import logoWhite from 'public/assets/images/logo/asrr-logo-spacing-small-white.svg'
 import { useTheme } from '../../context/theme/ThemeContext'
-
-const containerStyle = {
-	width: '100%',
-	height: '100%',
-};
+import mapStyles from './mapStyles'
 
 const center = {
 	lat: 52.011250, lng: 4.286770,
@@ -18,8 +14,8 @@ function Map (props) {
 
 	return (
 		<>
-			<LoadScript googleMapsApiKey="AIzaSyCSfB3HhJXkskqLOKlqaiSY3NK4VxlSjQQ">
-				<GoogleMap center={center} zoom={15} mapContainerClassName="map">
+			<LoadScript googleMapsApiKey="AIzaSyBMpKvbhfxhLV_S2t7zgHAZvlCyUKO8jwU">
+				<GoogleMap center={center} zoom={15} mapContainerClassName="map" options={{ styles: darkTheme.dark ? mapStyles.dark : mapStyles.light }}>
 					<>
 						<Marker icon={darkTheme.dark ? logoWhite : logo} position={center}/>
 					</>
