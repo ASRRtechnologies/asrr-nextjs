f68f03f90749a464e1d384a45a796f0e39799dee
Re-Angelo Jarvis <hesusdios@gmail.com>
2021-04-07 14:08:30 +0200

    upgrade react and nextjs

---
+++ src/components/utillities/landing/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface LandingProps {
+    title:string,
+    text:string,
+    image?:string,
+}
+
+function Landing(props:LandingProps) {
+    return (
+        <div>
+
+
+        </div>
+    );
+}
+
+export default Landing;
