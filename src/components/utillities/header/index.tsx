fced2d0ca4e9e8b91fd57a1216a5b24ce16113a0
Re-Angelo Jarvis <hesusdios@gmail.com>
2021-04-09 12:05:15 +0200

    remove index error

--- src/components/utillities/header/index.tsx
+++ src/components/utillities/header/index.tsx
@@ -1,3 +1,2 @@
-export { default as Button }  from "./Header";
-export { default as ActionButton }  from "./sub-components/ActionButton";
-export { default as NavigationButton }  from "./sub-components/NavigationButton";
+export { default as Header }  from "./Header";
+
