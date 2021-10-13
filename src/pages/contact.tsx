73b69bdc5c8ad68daa8681efb08abe8dcb44b7ad
Amar Ramdas <amar.ramdas@asrr.nl>
2021-10-13 21:55:47 +0200

    Quick cleanup

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import ContactPage from '../components/contact/Contact'
-import matter from 'gray-matter'
-import PageLayout from '@/modules/shared/layout/PageLayout'
-import Banner from "@/modules/shared/landing/Banner";
+import React from 'react';
+import ContactPage from '../components/contact/Contact';
+import matter from 'gray-matter';
+import PageLayout from '@/modules/shared/layout/PageLayout';
+import Banner from '@/modules/shared/landing/Banner';
 
 const SEOProps = {
-    title: "ASRR - Contact",
-    content: "ASRR Levert innovatieve software oplossingen met een specialisme in de bouw"
-}
+  title: 'ASRR - Contact',
+  content:
+    'ASRR Levert innovatieve software oplossingen met een specialisme in de bouw',
+};
 
-function Contact({content}) {
-    const basePath = `/content/contact/nl`;
+function Contact({ content }) {
+  const basePath = `/content/contact/nl`;
 
-    return (
-        <PageLayout noPreview={true} className="darkmodeContainer" {...SEOProps}>
-            <Banner {...content.landing} image={`${basePath}/${content.landing.image}`}/>
-            <ContactPage data={content}/>
-        </PageLayout>
-    )
+  return (
+    <PageLayout noPreview={true} className="darkmodeContainer" {...SEOProps}>
+      <Banner
+        {...content.landing}
+        image={`${basePath}/${content.landing.image}`}
+      />
+      <ContactPage data={content} />
+    </PageLayout>
+  );
 }
 
 export async function getStaticProps() {
-    //This is the portfolio page cms
-    // @ts-ignore
-    let data = await import(`public/content/contact/nl/contact.md`);
-    // let data = await import(`${BasePaths.CONTACT}/nl/contact.md`);
-    let content = matter(data.default).data;
+  const data = await import(`public/content/contact/nl/contact.md`);
+  const content = matter(data.default).data;
 
-    return {
-        props: {content},
-    }
+  return {
+    props: { content },
+  };
 }
 
-export default Contact
+export default Contact;
