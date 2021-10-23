31fc94d6769b315f164b45dea46fb870933a4314
Amar Ramdas <amar.ramdas@asrr.nl>
2021-10-23 23:06:00 +0200

    Justify text

--- src/components/modules/pages/about/AboutCards.tsx
+++ src/components/modules/pages/about/AboutCards.tsx
@@ -8,7 +8,10 @@ function AboutCards({ kernwaarde, basePath }) {
   return (
     <div>
       <h1 className="h3">{title}</h1>
-      <p className="no-margin" style={{ maxWidth: '400px' }}>
+      <p
+        className="no-margin"
+        style={{ maxWidth: '400px', textAlign: 'justify' }}
+      >
         {text}
       </p>
       {/*<LinkText*/}
