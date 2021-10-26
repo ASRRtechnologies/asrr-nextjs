b0e2795dddc665ac417bc618f601c9df972a5d33
Amar Ramdas <amar.ramdas@asrr.nl>
2021-10-27 00:19:39 +0200

    Upgrade to next 12

--- next.config.js
+++ next.config.js
@@ -1,22 +1,43 @@
+const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 const withVideos = require('next-videos');
 const withPWA = require('next-pwa');
 
-module.exports = withImages(
-  withVideos(
-    withPWA({
-      pwa: {
-        dest: 'public',
-        register: true,
-        skipWaiting: true,
+module.exports = withPlugins(
+  [
+    [
+      withVideos,
+      {
+        /* plugin config here ... */
       },
-      webpack: (cfg) => {
-        cfg.module.rules.push({
-          test: /\.md$/,
-          use: 'raw-loader',
-        });
-        return cfg;
+    ],
+    [
+      withImages,
+      {
+        images: {
+          formats: ['image/avif', 'image/webp'],
+        },
       },
-    })
-  )
+    ],
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public',
+          register: true,
+          skipWaiting: true,
+        },
+      },
+    ],
+  ],
+  {
+    webpack: (cfg) => {
+      cfg.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      });
+      return cfg;
+    },
+    swcMinify: true,
+  }
 );
--- src/components/modules/shared/landing/Banner.tsx
+++ src/components/modules/shared/landing/Banner.tsx
@@ -40,6 +40,7 @@ function Banner(props: BannerProps) {
           layout="fill"
           // height={600}
           // width={1200}
+          // placeholder="blur"
           priority
           quality={50}
         />
