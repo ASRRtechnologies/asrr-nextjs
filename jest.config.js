847778c5913cb7dcfc7c907de1420e2c2613ecc5
ReangeloJ <hesusdios@gmail.com>
2021-11-28 17:04:06 +0100

    Feat - Add test setup to repo

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { defaults } = require('jest-config');
 
 module.exports = {
-	...defaults,
-	preset: 'ts-jest',
-	collectCoverage: true,
-	coverageDirectory: 'js-coverage',
-	coverageReporters: ['lcov', 'text'],
-	globals: {
-		'ts-jest': {
-			tsConfig: 'tsconfig.test.json',
-		},
-	},
-	testPathIgnorePatterns: ['/cypress/', '__tests__/mockdata/'],
-	moduleNameMapper: {
-		'\\.(css|less|scss|sss|styl)$':
-			'<rootDir>/node_modules/jest-css-modules',
-	},
-	setupFiles: ['<rootDir>/jest/setupTest.js'],
-	transform: {
-		'\\.svg$': './jest/transformers/raw-loader.js',
-	},
-};
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
 
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': `<rootDir>/__mocks__/fileMock.js`,
+
+    // Handle module aliases
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+};
